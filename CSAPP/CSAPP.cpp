973628898b2a8bf2f63a54776375333f9c519e98
chinwe <chinwe122@gmail.com>
2015-06-23 22:11:52 +0800

    实现uadd_ok函数，增加测试代码

---
+++ CSAPP/CSAPP.cpp
@@ -0,0 +1,13 @@
+// CSAPP.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Header.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    test_uadd_ok();
+
+    return 0;
+}
+
---
+++ CSAPP/Header.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+//判断两个无符号整数相加是否溢出
+int uadd_ok(unsigned x, unsigned y);
+
+//uadd_ok测试函数
+void test_uadd_ok();
+
+#endif // !HEADER_H
+
